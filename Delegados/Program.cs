c8c55157c21228b404c382a8116822a38e6faf35
Juan Pablo <juanpablomato89@gmail.com>
2021-06-13 13:19:09 +0200

    first commit

---
+++ Delegados/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Delegados
+{
+    class Program
+    {
+        public delegate string Mostrar(string cadena);
+        static async Task Main(string[] args)
+        {
+            Mostrar mostrar = Show;
+            HacerAlgo(mostrar);
+        }
+
+        public static string Show(string cad)
+        {
+            return cad.ToUpper();
+        }
+
+        public static void HacerAlgo(Mostrar funcionfinal)
+        {
+            Console.WriteLine("Hago Algo");
+            Console.WriteLine(funcionfinal("Se envio desde otra funcion"));
+            Console.WriteLine("Hago algo mas");
+
+        }
+    }
+}
